ce326cafa264464830c37828c09616d481dfb684
Tamas Pallos <tamas.pallos@vincit.fi>
2021-10-07 15:07:42 +0300

    Create a postgres image that mocks the empty azure db
    When provisioning the jore4 database in Azure, we also fill it up with some initial data. E.g. create db users for microservices

---
+++ azuredbmock/00-initialize.sql
@@ -0,0 +1,7 @@
+
+-- Initializations, which are needed locally, but not in the cloud / prod environments,
+-- go here.
+
+-- These users are created from the jore4-deploy repository in cloud environments.
+CREATE USER xxx_db_auth_username_xxx PASSWORD 'xxx_db_auth_password_xxx';
+CREATE USER xxx_db_jore3importer_username_xxx PASSWORD 'xxx_db_jore3importer_password_xxx';
