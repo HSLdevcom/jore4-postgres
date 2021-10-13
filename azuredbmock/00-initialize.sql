16917a6866aeec5c297ccf4cbf333e85dfa1fa7d
Jörg Schummer <jorg.schummer@vincit.fi>
2021-10-13 12:01:32 +0300

    Adopt changes from deploy scripts to let Hasura use db as non-superuser

--- azuredbmock/00-initialize.sql
+++ azuredbmock/00-initialize.sql
@@ -5,3 +5,20 @@
 -- These users are created from the jore4-deploy repository in cloud environments.
 CREATE USER xxx_db_auth_username_xxx PASSWORD 'xxx_db_auth_password_xxx';
 CREATE USER xxx_db_jore3importer_username_xxx PASSWORD 'xxx_db_jore3importer_password_xxx';
+CREATE USER xxx_db_hasura_username_xxx PASSWORD 'xxx_db_hasura_password_xxx';
+
+-- Create the extensions used, see https://hasura.io/docs/latest/graphql/core/deployment/postgres-requirements.html
+-- Create the extensions in the public schema, since we'd need to give additional privileges ("use schema") to any
+-- user who wishes to use these in the future. Also, Hasura would require additional setup to be able to use the
+-- extensions from another schema.
+CREATE EXTENSION IF NOT EXISTS pgcrypto;
+CREATE EXTENSION IF NOT EXISTS postgis;
+
+-- create the schema required by the hasura system
+CREATE SCHEMA IF NOT EXISTS hdb_catalog;
+
+-- make the user an owner of system schemas
+ALTER SCHEMA hdb_catalog OWNER TO xxx_db_hasura_username_xxx;
+
+-- allow hasura to create new schemas in migrations
+GRANT CREATE ON DATABASE postgres TO xxx_db_hasura_username_xxx;
