c0cc133192157c6840acd3cd5ecb4164793991ed
Teemu Mäkinen <teemu.makinen@vincit.fi>
2023-04-11 12:50:24 +0300

    Initialize stop database for tiamat

--- azuredbmock/00-initialize.sql
+++ azuredbmock/00-initialize.sql
@@ -6,6 +6,7 @@
 CREATE USER xxx_db_auth_username_xxx PASSWORD 'xxx_db_auth_password_xxx';
 CREATE USER xxx_db_jore3importer_username_xxx PASSWORD 'xxx_db_jore3importer_password_xxx';
 CREATE USER xxx_db_hasura_username_xxx PASSWORD 'xxx_db_hasura_password_xxx';
+CREATE USER xxx_db_tiamat_username_xxx PASSWORD 'xxx_db_tiamat_password_xxx';
 
 -- Create the extensions used, see https://hasura.io/docs/latest/graphql/core/deployment/postgres-requirements.html
 -- Create the extensions in the public schema, since we'd need to give additional privileges ("use schema") to any
@@ -30,11 +31,24 @@ GRANT ALL ON DATABASE xxx_db_jore3importer_name_xxx TO xxx_db_jore3importer_user
 CREATE DATABASE xxx_db_timetables_name_xxx;
 GRANT CREATE ON DATABASE xxx_db_timetables_name_xxx TO xxx_db_hasura_username_xxx;
 
+-- create database for stop places and give ALL privileges to Tiamat in it
+CREATE DATABASE xxx_db_tiamat_name_xxx;
+GRANT ALL ON DATABASE xxx_db_tiamat_name_xxx TO xxx_db_tiamat_username_xxx;
+
 -- switch database context to timetables db to be able to add extensions there
 \connect xxx_db_timetables_name_xxx;
 CREATE EXTENSION IF NOT EXISTS pgcrypto;
 CREATE EXTENSION IF NOT EXISTS postgis;
 CREATE EXTENSION IF NOT EXISTS btree_gist;
+
+-- switch database context to stop db to initialize it to the state where tiamat can use it
+\connect xxx_db_tiamat_name_xxx;
+CREATE SCHEMA topology;
+ALTER SCHEMA topology OWNER TO xxx_db_tiamat_username_xxx;
+CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
+CREATE EXTENSION IF NOT EXISTS postgis WITH SCHEMA public;
+CREATE EXTENSION IF NOT EXISTS postgis_topology WITH SCHEMA topology;
+CREATE EXTENSION IF NOT EXISTS pg_trgm WITH SCHEMA public;
 
 -- interval outputs by default are using the sql format ('3 4:05:06'). Here we are switching to ISO 8601 format ('P3DT4H5M6S')
 ALTER DATABASE xxx_db_timetables_name_xxx SET intervalstyle = 'iso_8601';
