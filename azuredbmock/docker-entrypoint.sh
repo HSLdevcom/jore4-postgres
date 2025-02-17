93491f3d79978acad1aefdd999a266f875681ba9
Jarkko Kaura <jarkko.kaura@vincit.fi>
2025-02-17 10:23:56 +0200

    Change the location of JORE4-specific shell scripts for Azure mock database image.

--- azuredbmock/docker-entrypoint.sh
+++ azuredbmock/docker-entrypoint.sh
@@ -3,16 +3,16 @@
 set -Eeuo pipefail
 
 # read the secrets to environment variables
-source /tmp/read-secrets.sh
+source /jore4/scripts/read-secrets.sh
 
 # copy the migrations scripts to the docker-entrypoint-initdb.d folder so that the postgres entrypoint
 # executes them (together with the other migrations that might have been mapped as a volume)
 mkdir -p /docker-entrypoint-initdb.d
-cp /migrations/* /docker-entrypoint-initdb.d/
+cp /jore4/migrations/* /docker-entrypoint-initdb.d/
 
 # replace placeholders with secrets within the all migration SQL scripts
 SECRET_STORE_BASE_PATH="${SECRET_STORE_BASE_PATH:-/run/secrets}"
-REPLACE_PLACEHOLDERS_SCRIPT='/scripts/replace-placeholders-in-sql-schema-migrations.sh'
+REPLACE_PLACEHOLDERS_SCRIPT='/jore4/scripts/replace-placeholders-in-sql-schema-migrations.sh'
 MIGRATIONS_DIR="/docker-entrypoint-initdb.d/"
 "${REPLACE_PLACEHOLDERS_SCRIPT}" "${SECRET_STORE_BASE_PATH}" "${MIGRATIONS_DIR}"
 
