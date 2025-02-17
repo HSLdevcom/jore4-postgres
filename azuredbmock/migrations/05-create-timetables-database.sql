8c128f7bc0ccc9368ec7b2695b7c53d326e55efc
Jarkko Kaura <jarkko.kaura@vincit.fi>
2025-02-17 10:41:37 +0200

    Update PostGIS base Docker image for Azure mock database: `12-3.1` -> `15-3.5`
    In PostgreSQL 15, full schema privileges must be separately granted to users
    performing database migrations.
    
    Make the JORE4 administrator role the owner of the public schema for all
    databases. In PostgreSQL 12, this was the default.

--- azuredbmock/migrations/05-create-timetables-database.sql
+++ azuredbmock/migrations/05-create-timetables-database.sql
@@ -1,6 +1,7 @@
--- Create database and allow Hasura to create new schemas in it.
 CREATE DATABASE xxx_db_timetables_name_xxx;
-GRANT CREATE ON DATABASE xxx_db_timetables_name_xxx TO xxx_db_hasura_username_xxx;
+
+-- Allow Hasura to connect and create new schemas.
+GRANT CONNECT, CREATE ON DATABASE xxx_db_timetables_name_xxx TO xxx_db_hasura_username_xxx;
 
 -- Interval outputs by default are using the sql format ('3 4:05:06'). Here we
 -- are switching to ISO 8601 format ('P3DT4H5M6S').
@@ -9,5 +10,27 @@ ALTER DATABASE xxx_db_timetables_name_xxx SET intervalstyle = 'iso_8601';
 -- Switch database context to be able to add extensions there.
 \connect xxx_db_timetables_name_xxx;
 
+-- Make the JORE4 admin role the owner of the public schema.
+ALTER SCHEMA public OWNER TO CURRENT_USER;
+
 CREATE EXTENSION IF NOT EXISTS btree_gist WITH SCHEMA public;
 CREATE EXTENSION IF NOT EXISTS pgcrypto WITH SCHEMA public;
+
+-- Grant required privileges in the public schema to Hasura.
+GRANT ALL ON SCHEMA public TO xxx_db_hasura_username_xxx;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO xxx_db_hasura_username_xxx;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO xxx_db_hasura_username_xxx;
+
+-- Grant select permissions on information_schema and pg_catalog to Hasura.
+GRANT SELECT ON ALL TABLES IN SCHEMA information_schema TO xxx_db_hasura_username_xxx;
+GRANT SELECT ON ALL TABLES IN SCHEMA pg_catalog TO xxx_db_hasura_username_xxx;
+
+-- Allow the timetables-api role to connect to the timetables database.
+GRANT CONNECT ON DATABASE xxx_db_timetables_name_xxx TO xxx_db_timetables_api_username_xxx;
+
+-- Grant the timetables-api role to access the public schema and the objects and
+-- functions created by extensions. Other schema-specific privileges are granted
+-- in Hasura migrations.
+GRANT USAGE ON SCHEMA public TO xxx_db_timetables_api_username_xxx;
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO xxx_db_timetables_api_username_xxx;
+GRANT EXECUTE ON ALL FUNCTIONS IN SCHEMA public TO xxx_db_timetables_api_username_xxx;
