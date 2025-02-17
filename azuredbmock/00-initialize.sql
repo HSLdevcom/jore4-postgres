c142d4d0e7595df13c6152808b42cbc0e9c603bd
Jarkko Kaura <jarkko.kaura@vincit.fi>
2025-02-17 20:33:02 +0200

    Replace one SQL comment line with up-to-date information.

--- azuredbmock/00-initialize.sql
+++ azuredbmock/00-initialize.sql
@@ -2,7 +2,8 @@
 -- Initializations, which are needed locally, but not in the cloud / prod environments,
 -- go here.
 
--- These users are created from the jore4-deploy repository in cloud environments.
+-- These database roles are also created in the azure-infra-jore4aks (Azure
+-- DevOps) repository.
 CREATE USER xxx_db_auth_username_xxx PASSWORD 'xxx_db_auth_password_xxx';
 CREATE USER xxx_db_jore3importer_username_xxx PASSWORD 'xxx_db_jore3importer_password_xxx';
 CREATE USER xxx_db_hasura_username_xxx PASSWORD 'xxx_db_hasura_password_xxx';
